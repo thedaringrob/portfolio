130cecec235baf74dbce657cf028eaf5f9fac5ec
Robert Crozier <crozierr2@gmail.com>
2018-07-08 15:10:51 -0400

    media grid in place

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
       <div class="content-wrap">
         <h2>Media</h2>
 
-
+        <?php require 'media.php' ?>
 
       </div>
     </div>
@@ -34,14 +34,14 @@
 
 
     <div id="about" class="contentpanel">
-      <div class="content-wrap">
+      <div class="content-wrap hidescroll">
         <h2>About</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       </div>
     </div>
 
-    <div id="contact" class="contentpanel">
-      <div class="content-wrap">
+    <div id="contact" class="contentpanel ">
+      <div class="content-wrap hidescroll">
         <?php require 'contact.php' ?>
       </div>
     </div>
---
+++ media.php
@@ -0,0 +1,37 @@
+<div class="flex-grid">
+  <div class="span3">
+    <img class="" src="assets/564.jpg" alt="">
+  </div>
+  <div class="span3">
+    <p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</p>
+  </div>
+  <div class="span6">
+    <p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</p>
+  </div>
+  <div class="span6">
+    <img class="" src="assets/731.jpg" alt="">
+  </div>
+
+
+  <div class="span3">
+    <p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</p>
+  </div>
+  <div class="span3">
+    <img class="" src="assets/433.jpg" alt="">
+  </div>
+
+
+</div>
+
+
+
+<!-- quotes -->
+<!-- Robert Crozier is the essence of British propriety and punctiliousness as Fogg
+-80 days - creative loafing perry tannenbaum -->
+
+<!-- Robert Crozier played the autobiographical narrator in Neil Simon’s comedy memoir, which deftly revisits the comedy and the crucible of basic training during WW2. In Crozier’s beautifully calibrated performance, Eugene Morris Jerome became a perfect mix of candid likability and shy reserve.
+biloxi blues creative loafing perry
+-->
+
+<!-- The dance Watts and Crozier do around each other requires cultivating a complex chemistry where domination and subjection are constantly flickering on and off. Watts alternates between poignant mommy and devastating siren while Crozier is equally adept at zigzagging between efficient professional and romantic naif.
+death tax clclt tannenbaum-->
